2309e155bf3b3a2fe8b08f0c3a3255d83b0c16b9
salma861 <saadaouisalma30@gmail.com>
2022-05-07 13:30:34 +0200

    kjl

---
+++ src/Entity/Produit.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ProduitRepository;
+
+/**
+ * Produit
+ *
+ * @ORM\Table(name="produit")
+ * @ORM\Entity(repositoryClass=ProduitRepository::class)
+ */
+class Produit
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="IDProduit", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idproduit;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Designation", type="string", length=255, nullable=false)
+     */
+    private $designation;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Description", type="text", length=65535, nullable=false)
+     */
+    private $description;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="Image", type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="QuantiteEnStock", type="integer", nullable=false)
+     */
+    private $quantiteenstock;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="prixAchatUnit", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $prixachatunit;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="prixVenteUnit", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $prixventeunit;
+
+    /**
+     * @var bool|null
+     *
+     * @ORM\Column(name="StatusStock", type="boolean", nullable=true)
+     */
+    private $statusstock;
+
+    public function getIdproduit(): ?int
+    {
+        return $this->idproduit;
+    }
+
+    public function getDesignation(): ?string
+    {
+        return $this->designation;
+    }
+
+    public function setDesignation(string $designation): self
+    {
+        $this->designation = $designation;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getQuantiteenstock(): ?int
+    {
+        return $this->quantiteenstock;
+    }
+
+    public function setQuantiteenstock(int $quantiteenstock): self
+    {
+        $this->quantiteenstock = $quantiteenstock;
+
+        return $this;
+    }
+
+    public function getPrixachatunit(): ?float
+    {
+        return $this->prixachatunit;
+    }
+
+    public function setPrixachatunit(float $prixachatunit): self
+    {
+        $this->prixachatunit = $prixachatunit;
+
+        return $this;
+    }
+
+    public function getPrixventeunit(): ?float
+    {
+        return $this->prixventeunit;
+    }
+
+    public function setPrixventeunit(float $prixventeunit): self
+    {
+        $this->prixventeunit = $prixventeunit;
+
+        return $this;
+    }
+
+    public function getStatusstock(): ?bool
+    {
+        return $this->statusstock;
+    }
+
+    public function setStatusstock(?bool $statusstock): self
+    {
+        $this->statusstock = $statusstock;
+
+        return $this;
+    }
+public function _toString(){
+    return (string)$this->id;
+}
+
+}
